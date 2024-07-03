05936f715707b069df032a5c95b829b8998164bf
lauichan <nauichan@outlook.com>
2024-07-03 21:50:16 +0900

    feat: Table에서 우클릭 메뉴 추가. 우클릭 position 출력

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
+import ContextMenu, { MousePosition } from "../ContextMenu/ContextMenu";
+
 function Table({ table }: { table: string[][] }) {
+  const [contextMenu, setContextMenu] = useState<MousePosition>(null);
+
+  const handleContextMenu = (
+    e: React.MouseEvent<HTMLTableCellElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setContextMenu({
+      x: e.pageX,
+      y: e.pageY,
+    });
+  };
+
   return (
-    <table>
-      <tbody>
-        {table.map((row, rowIdx) => (
-          <tr key={`row-${rowIdx}`}>
-            {row.map((column, colIdx) => (
-              <td key={`${rowIdx}-${colIdx}`}>{column}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table>
+        <tbody>
+          {table.map((row, rowIdx) => (
+            <tr key={`row-${rowIdx}`}>
+              {row.map((column, colIdx) => (
+                <td
+                  key={`${rowIdx}-${colIdx}`}
+                  onContextMenu={handleContextMenu}
+                >
+                  {column}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {contextMenu && <ContextMenu position={contextMenu} />}
+    </>
   );
 }
 
