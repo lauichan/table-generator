35a6e1c34efff504389e59e496636eca9b535736
lauichan <nauichan@outlook.com>
2024-07-18 17:07:08 +0900

    table Raw HTML 생성 유틸함수

---
+++ src/utils/createHtml.ts
@@ -0,0 +1,13 @@
+export const createTableHtml = (table: string[][]) => {
+  const html = table.map((row) => createRowHtml(row)).join("")
+  return `<table>${html}</table>`
+};
+
+const createRowHtml = (row: string[]) => {
+  const html = row.map((define) => createDefineHtml(define)).join("")
+  return `<tr>${html}</tr>`
+};
+
+const createDefineHtml = (define: string) => {
+  return `<td>${define}</td>`
+}
