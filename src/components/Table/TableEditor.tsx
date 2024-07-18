2b4f4a03af762254a8bd205e2d07f9d5244cbdda
lauichan <nauichan@outlook.com>
2024-07-18 21:49:30 +0900

    텍스트 편집 contentEditable로 임시 구현

--- src/components/Table/TableEditor.tsx
+++ src/components/Table/TableEditor.tsx
@@ -1,30 +1,35 @@
 import Table from "./Table";
+import TableController from "./TableController";
 import styles from "./TableEditor.module.css";
 
-type TableProps = {
+type TableEditorProps = {
   table: string[][];
   addRowColumn: (rowCount: number, columnCount: number) => void;
+  setTableText: (rowIdx: number, colIdx: number, text: string) => void;
 };
 
-function TableEditor({ table, addRowColumn }: TableProps) {
+function TableEditor({ table, addRowColumn, setTableText }: TableEditorProps) {
   return (
-    <div className={styles.table}>
-      <Table table={table} />
-      <button className={styles.add_col} onClick={() => addRowColumn(0, 1)}>
-        +
-      </button>
-      <button className={styles.add_row} onClick={() => addRowColumn(1, 0)}>
-        +
-      </button>
-      <button
-        className={styles.add_col_and_row}
-        onClick={() => {
-          addRowColumn(1, 1);
-        }}
-      >
-        +
-      </button>
-    </div>
+    <>
+      <TableController />
+      <div className={styles.table}>
+        <Table table={table} setTableText={setTableText} />
+        <button className={styles.add_col} onClick={() => addRowColumn(0, 1)}>
+          +
+        </button>
+        <button className={styles.add_row} onClick={() => addRowColumn(1, 0)}>
+          +
+        </button>
+        <button
+          className={styles.add_col_and_row}
+          onClick={() => {
+            addRowColumn(1, 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+    </>
   );
 }
 
