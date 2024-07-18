829e1457b4ed3c91ce50f4c33ebe7339a8be2875
lauichan <nauichan@outlook.com>
2024-07-18 22:39:31 +0900

    행 열 입력값으로 제어하는 form 생성

--- src/components/Table/TableController.tsx
+++ src/components/Table/TableController.tsx
@@ -3,8 +3,8 @@ import styles from "./TableController.module.css";
 function TableController() {
   return (
     <form className={styles.controller}>
-      <input></input>
-      <input></input>
+      <input placeholder="행"></input>
+      <input placeholder="열"></input>
       <button>생성</button>
     </form>
   );
