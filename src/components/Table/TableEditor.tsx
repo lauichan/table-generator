b9225813a5aa67a5edbb745db0c20339ed45d51c
lauichan <nauichan@outlook.com>
2024-07-19 18:44:09 +0900

    변경된 함수이름, class 이름 가져오기 수정

--- src/components/Table/TableEditor.tsx
+++ src/components/Table/TableEditor.tsx
@@ -1,31 +1,28 @@
 import Table from "./Table";
-import TableController from "./TableController";
+import TableSizer from "./TableSizer";
 import styles from "./TableEditor.module.css";
 
 type TableEditorProps = {
   table: string[][];
-  addRowColumn: (rowCount: number, columnCount: number) => void;
+  setRowColumn: (rowCount: number, columnCount: number) => void;
   setTableText: (rowIdx: number, colIdx: number, text: string) => void;
 };
 
-function TableEditor({ table, addRowColumn, setTableText }: TableEditorProps) {
+function TableEditor({ table, setRowColumn, setTableText }: TableEditorProps) {
+  const tableRowCol = { row: table.length, col: table[0].length };
+
   return (
     <>
-      <TableController />
+      <TableSizer tableRowCol={tableRowCol} setRowColumn={setRowColumn} />
       <div className={styles.table}>
         <Table table={table} setTableText={setTableText} />
-        <button className={styles.add_col} onClick={() => addRowColumn(0, 1)}>
+        <button className={styles["right"]} onClick={() => setRowColumn(0, 1)}>
           +
         </button>
-        <button className={styles.add_row} onClick={() => addRowColumn(1, 0)}>
+        <button className={styles["bottom"]} onClick={() => setRowColumn(1, 0)}>
           +
         </button>
-        <button
-          className={styles.add_col_and_row}
-          onClick={() => {
-            addRowColumn(1, 1);
-          }}
-        >
+        <button className={styles["bottom-right"]} onClick={() => setRowColumn(1, 1)}>
           +
         </button>
       </div>
