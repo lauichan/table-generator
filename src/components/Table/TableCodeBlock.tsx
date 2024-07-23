c4c35beca4a7d6d9eba0c47716608e8d3ff5f34a
lauichan <nauichan@outlook.com>
2024-07-23 21:42:15 +0900

    feat: 코드 최소화 체크박스 기능

--- src/components/Table/TableCodeBlock.tsx
+++ src/components/Table/TableCodeBlock.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { useTableStore } from "../../store/useTableStore";
 import createTableHtml from "../../utils/createHtml";
 import formatHtml from "../../utils/formatHtml";
 
 function TableCodeBlock() {
   const table = useTableStore((state) => state.table);
+  const [isMinified, setIsMinified] = useState(false);
+  const tableHtml = createTableHtml(table);
 
-  return <pre>{formatHtml(createTableHtml(table))}</pre>;
+  const handleMinify = () => {
+    setIsMinified((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div>
+        <input id="minify" type="checkbox" checked={isMinified} onChange={handleMinify} />
+        <label htmlFor="minify">코드 최소화</label>
+      </div>
+      <pre>{isMinified ? tableHtml : formatHtml(tableHtml)}</pre>
+    </>
+  );
 }
 
 export default TableCodeBlock;
