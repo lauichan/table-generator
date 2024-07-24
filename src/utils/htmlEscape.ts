71db7a1622d107b9bee212344647980a331308b1
lauichan <nauichan@outlook.com>
2024-07-24 17:13:26 +0900

    escape html special char

---
+++ src/utils/htmlEscape.ts
@@ -0,0 +1,20 @@
+const htmlEscape = (str: string) => {
+  return str.replace(/[&<>"']/g, (match) => {
+    switch (match) {
+      case "&":
+        return "&amp;";
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case '"':
+        return "&quot;";
+      case "'":
+        return "&#39;";
+      default:
+        return match;
+    }
+  });
+};
+
+export default htmlEscape
