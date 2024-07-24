71ddd7003f7ab4f9c89044dd2ac8c56c561d4aae
lauichan <nauichan@outlook.com>
2024-07-24 20:50:43 +0900

    feat: 코드 블럭, 미리보기, 코드 복사 기능

--- src/components/Table/TableCodeBlock.tsx
+++ src/components/Table/TableCodeBlock.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useTableStore } from "../../store/useTableStore";
 import createTableHtml from "../../utils/createHtml";
 import formatHtml from "../../utils/formatHtml";
 import sanitizeHtml from "../../utils/sanitizeHtml";
+import styles from "./TableCodeBlock.module.css";
 
 function TableCodeBlock() {
-  const [tabContent, setTabContent] = useState<"code" | "preview">("code");
   const [isMinified, setIsMinified] = useState(false);
+  const codeRef = useRef<HTMLElement>(null);
   const table = useTableStore((state) => state.table);
   const tableHtml = createTableHtml(table);
   const code = sanitizeHtml(isMinified ? tableHtml : formatHtml(tableHtml));
@@ -15,34 +16,28 @@ function TableCodeBlock() {
     setIsMinified((prev) => !prev);
   };
 
-  const handleChangeMode = (content: "code" | "preview") => {
-    setTabContent(content);
+  const handleCopyCode = () => {
+    const text = codeRef.current;
+    try {
+      if (!text) throw "실패";
+      navigator.clipboard.writeText(text.textContent || "");
+      alert("클립보드에 복사되었습니다.");
+    } catch (error) {
+      alert("클립보드 복사에 실패하였습니다.");
+    }
   };
 
   return (
-    <>
-      <div>
-        <input id="minify" type="checkbox" checked={isMinified} onChange={handleMinify} />
-        <label htmlFor="minify">코드 최소화</label>
-      </div>
-      <div>
-        <button onClick={() => handleChangeMode("code")} disabled={tabContent === "code"}>
-          코드
-        </button>
-        <button onClick={() => handleChangeMode("preview")} disabled={tabContent === "preview"}>
-          미리보기
-        </button>
-      </div>
-      <div>
-        {tabContent === "preview" ? (
-          <div dangerouslySetInnerHTML={{ __html: code }} />
-        ) : (
-          <pre>
-            <code>{code}</code>
-          </pre>
-        )}
-      </div>
-    </>
+    <div className={styles["tab"]}>
+      <button onClick={handleCopyCode}>코드복사</button>
+      <input id="minify" type="checkbox" checked={isMinified} onChange={handleMinify} />
+      <label htmlFor="minify">코드 최소화</label>
+      <pre className={styles["html"]}>
+        <code ref={codeRef}>{code}</code>
+      </pre>
+      <strong>미리보기</strong>
+      <div className={styles["preview"]} dangerouslySetInnerHTML={{ __html: code }} />
+    </div>
   );
 }
 
