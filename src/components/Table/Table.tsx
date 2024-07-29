03d3f1adcbb1a25b75c65d680944cbfbe793a660
lauichan <nauichan@outlook.com>
2024-07-29 21:24:35 +0900

    feat: thead, tfoot 기능 추가

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -8,14 +8,23 @@ function Table({ table }: { table: string[][] }) {
   const [contextMenu, setContextMenu] = useState<MousePosition>(null);
   const cellRefs = useRef<HTMLTableCellElement[][]>([]);
   const setTableText = useTableStore((state) => state.setTableText);
+  const thead = useTableStore((state) => state.thead);
+  const tfoot = useTableStore((state) => state.tfoot);
+  const tbody = thead
+    ? tfoot
+      ? table.slice(1, table.length - 1)
+      : table.slice(1)
+    : tfoot
+    ? table.slice(0, table.length - 1)
+    : table;
 
-  const handleContextMenu = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>) => {
-    e.preventDefault();
-    setContextMenu({
-      x: e.pageX,
-      y: e.pageY,
-    });
-  };
+  // const handleContextMenu = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>) => {
+  //   e.preventDefault();
+  //   setContextMenu({
+  //     x: e.pageX,
+  //     y: e.pageY,
+  //   });
+  // };
 
   const handleFocusOut = (
     e: FocusEvent<HTMLTableCellElement, Element>,
@@ -53,29 +62,79 @@ function Table({ table }: { table: string[][] }) {
   return (
     <>
       <table>
+        {thead ? (
+          <thead>
+            <tr>
+              {table[0].map((col, colIdx) => (
+                <th
+                  ref={(el: HTMLTableCellElement) => {
+                    if (!cellRefs.current[0]) {
+                      cellRefs.current[0] = [];
+                    }
+                    cellRefs.current[0][colIdx] = el;
+                  }}
+                  key={`header-${colIdx}`}
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleFocusOut(e, 0, colIdx)}
+                  onKeyDown={(e) => handleKeyDown(e, 0, colIdx)}
+                  // onContextMenu={handleContextMenu}
+                  dangerouslySetInnerHTML={{ __html: sanitizeHtml(col) }}
+                />
+              ))}
+            </tr>
+          </thead>
+        ) : (
+          <></>
+        )}
         <tbody>
-          {table.map((row, rowIdx) => (
+          {tbody.map((row, rowIdx) => (
             <tr key={`row-${rowIdx}`}>
               {row.map((column, colIdx) => (
                 <td
                   ref={(el: HTMLTableCellElement) => {
-                    if (!cellRefs.current[rowIdx]) {
-                      cellRefs.current[rowIdx] = [];
+                    if (!cellRefs.current[rowIdx + (thead ? 1 : 0)]) {
+                      cellRefs.current[rowIdx + (thead ? 1 : 0)] = [];
                     }
-                    cellRefs.current[rowIdx][colIdx] = el;
+                    cellRefs.current[rowIdx + (thead ? 1 : 0)][colIdx] = el;
                   }}
                   key={`${rowIdx}-${colIdx}`}
                   contentEditable
                   suppressContentEditableWarning
-                  onBlur={(e) => handleFocusOut(e, rowIdx, colIdx)}
-                  onKeyDown={(e) => handleKeyDown(e, rowIdx, colIdx)}
-                  onContextMenu={handleContextMenu}
+                  onBlur={(e) => handleFocusOut(e, rowIdx + (thead ? 1 : 0), colIdx)}
+                  onKeyDown={(e) => handleKeyDown(e, rowIdx + (thead ? 1 : 0), colIdx)}
+                  // onContextMenu={handleContextMenu}
                   dangerouslySetInnerHTML={{ __html: sanitizeHtml(column) }}
                 />
               ))}
             </tr>
           ))}
         </tbody>
+        {tfoot ? (
+          <tfoot>
+            <tr>
+              {table.slice(-1)[0].map((col, colIdx) => (
+                <td
+                  ref={(el: HTMLTableCellElement) => {
+                    if (!cellRefs.current[table.length - 1]) {
+                      cellRefs.current[table.length - 1] = [];
+                    }
+                    cellRefs.current[table.length - 1][colIdx] = el;
+                  }}
+                  key={`footer-${colIdx}`}
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={(e) => handleFocusOut(e, table.length - 1, colIdx)}
+                  onKeyDown={(e) => handleKeyDown(e, table.length - 1, colIdx)}
+                  // onContextMenu={handleContextMenu}
+                  dangerouslySetInnerHTML={{ __html: sanitizeHtml(col) }}
+                />
+              ))}
+            </tr>
+          </tfoot>
+        ) : (
+          <></>
+        )}
       </table>
       {contextMenu && <ContextMenu position={contextMenu} />}
     </>
