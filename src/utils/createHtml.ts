aa699e53643ee345b24463f2b4b784ac6857a60c
lauichan <nauichan@outlook.com>
2024-07-29 21:28:03 +0900

    feat: 코드 html 추가된 기능에 맞게 생성

--- src/utils/createHtml.ts
+++ src/utils/createHtml.ts
@@ -1,6 +1,31 @@
-const createTableHtml = (table: string[][]) => {
-  const html = table.map((row) => createRowHtml(row)).join("")
-  return `<table>${html}</table>`
+const createTableHtml = (table: string[][], thead: boolean, tfoot: boolean) => {
+  let theadHtml = '';
+  let tfootHtml = '';
+  let tbodyHtml = '';
+
+  if (thead) {
+    theadHtml = `<thead>${createRowHtml(table[0])}</thead>`;
+  }
+
+  if (tfoot) {
+    tfootHtml = `<tfoot>${createRowHtml(table[table.length - 1])}</tfoot>`;
+  }
+
+  const tbody = thead
+    ? tfoot
+      ? table.slice(1, table.length - 1)
+      : table.slice(1)
+    : tfoot
+    ? table.slice(0, table.length - 1)
+    : table;
+
+  if (thead || tfoot) {
+    tbodyHtml = `<tbody>${tbody.map((row) => createRowHtml(row)).join('')}</tbody>`;
+  } else {
+    tbodyHtml = tbody.map((row) => createRowHtml(row)).join('')
+  }
+
+  return `<table>${theadHtml}${tbodyHtml}${tfootHtml}</table>`;
 };
 
 const createRowHtml = (row: string[]) => {
