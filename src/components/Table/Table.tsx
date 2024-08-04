227d10817e6cea5681eb147b2fa937577a921ef5
lauichan <nauichan@outlook.com>
2024-08-04 22:53:45 +0900

    코드 분리

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,14 +1,19 @@
-import { FocusEvent, KeyboardEvent, useRef } from "react";
-import ContextMenu from "../ContextMenu/ContextMenu";
-import { type CellType, useTableStore } from "../../store/useTableStore";
-import { useOptionStore } from "../../store/useOptionStore";
-import htmlEscape from "../../utils/htmlEscape";
-import Cell from "./Cell/Cell";
-import useContextMenu from "../../hooks/useContextMenu";
+import { FocusEvent } from "react";
 import { useShallow } from "zustand/react/shallow";
+import Cell from "./Cell/Cell";
+import ContextMenu from "../ContextMenu/ContextMenu";
+import { useOptionStore } from "../../store/useOptionStore";
+import { type CellType, useTableStore } from "../../store/useTableStore";
+import useContextMenu from "../../hooks/useContextMenu";
+import useSelectCells from "../../hooks/useSelectCells";
+import useArrowNavigate from "../../hooks/useArrowNavigate";
+import htmlEscape from "../../utils/htmlEscape";
+import isSelectedCell from "../../utils/isSelectedCell";
 
 function Table({ table }: { table: CellType[][] }) {
   const { contextMenuRef, handleContextMenu, contextMenu } = useContextMenu();
+  const { range, handleMouseDown, handleMouseUp } = useSelectCells();
+  const { cellRefs, handleKeyDown } = useArrowNavigate(table);
   const [thead, tfoot] = useOptionStore(useShallow((state) => [state.thead, state.tfoot]));
   const setTableText = useTableStore((state) => state.setTableText);
 
@@ -23,32 +28,6 @@ function Table({ table }: { table: CellType[][] }) {
     setTableText(rowIdx, colIdx, text);
   };
 
-  const cellRefs = useRef<HTMLTableCellElement[][]>([]);
-
-  const handleKeyDown = (
-    e: KeyboardEvent<HTMLTableCellElement>,
-    rowIdx: number,
-    colIdx: number
-  ) => {
-    if (e.key === "ArrowRight") {
-      if (colIdx < table[0].length - 1) {
-        cellRefs.current[rowIdx][colIdx + 1].focus();
-      }
-    } else if (e.key === "ArrowLeft") {
-      if (colIdx > 0) {
-        cellRefs.current[rowIdx][colIdx - 1].focus();
-      }
-    } else if (e.key === "ArrowDown") {
-      if (rowIdx < table.length - 1) {
-        cellRefs.current[rowIdx + 1][colIdx].focus();
-      }
-    } else if (e.key === "ArrowUp") {
-      if (rowIdx > 0) {
-        cellRefs.current[rowIdx - 1][colIdx].focus();
-      }
-    }
-  };
-
   return (
     <>
       <table ref={contextMenuRef}>
@@ -59,12 +38,15 @@ function Table({ table }: { table: CellType[][] }) {
                 <Cell
                   key={`header-${colIdx}`}
                   {...cell}
+                  selected={isSelectedCell(range, 0, colIdx)}
                   cellRefs={cellRefs}
                   rowIdx={0}
                   colIdx={colIdx}
                   handleFocusOut={handleFocusOut}
                   handleKeyDown={handleKeyDown}
                   handleContextMenu={handleContextMenu}
+                  handleMouseDown={handleMouseDown}
+                  handleMouseUp={handleMouseUp}
                 />
               ))}
             </tr>
@@ -79,12 +61,15 @@ function Table({ table }: { table: CellType[][] }) {
                 <Cell
                   key={`tbody-${rowIdx}-${colIdx}`}
                   {...cell}
+                  selected={isSelectedCell(range, rowIdx, colIdx)}
                   cellRefs={cellRefs}
                   rowIdx={rowIdx}
                   colIdx={colIdx}
                   handleFocusOut={handleFocusOut}
                   handleKeyDown={handleKeyDown}
                   handleContextMenu={handleContextMenu}
+                  handleMouseDown={handleMouseDown}
+                  handleMouseUp={handleMouseUp}
                 />
               ))}
             </tr>
@@ -97,12 +82,15 @@ function Table({ table }: { table: CellType[][] }) {
                 <Cell
                   key={`tfoot-${colIdx}`}
                   {...cell}
+                  selected={isSelectedCell(range, table.length - 1, colIdx)}
                   cellRefs={cellRefs}
                   rowIdx={table.length - 1}
                   colIdx={colIdx}
                   handleFocusOut={handleFocusOut}
                   handleKeyDown={handleKeyDown}
                   handleContextMenu={handleContextMenu}
+                  handleMouseDown={handleMouseDown}
+                  handleMouseUp={handleMouseUp}
                 />
               ))}
             </tr>
@@ -112,6 +100,7 @@ function Table({ table }: { table: CellType[][] }) {
         )}
       </table>
       {contextMenu && <ContextMenu position={contextMenu} />}
+      {JSON.stringify(range)}
     </>
   );
 }
