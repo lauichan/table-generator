49bd69e35f872192a62a784df9e78bd96cc8f3e9
lauichan <nauichan@outlook.com>
2024-08-06 21:05:36 +0900

    fix: 셀 선택할때 우클릭 선택(컨텍스트 메뉴)은 제외

--- src/hooks/useSelectCells.ts
+++ src/hooks/useSelectCells.ts
@@ -1,4 +1,6 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent } from "react";
+import { useShallow } from "zustand/react/shallow";
+import { useSelectCellsStore } from "../store/useSelectCellsStore";
 
 export type SelectedRange = {
   endRow?: number | undefined;
@@ -8,30 +10,33 @@ export type SelectedRange = {
 };
 
 const useSelectCells = () => {
-  const [startIdx, setStartIdx] = useState<{ startRow: number; startCol: number } | null>(null);
-  const [endIdx, setEndIdx] = useState<{ endRow: number; endCol: number } | null>(null);
+  const [startIdx, endIdx, setStartIdx, setEndIdx] = useSelectCellsStore(
+    useShallow((state) => [state.startIdx, state.endIdx, state.setStartIdx, state.setEndIdx])
+  );
 
-  const handleMouseDown = (_: MouseEvent<HTMLTableCellElement>, rowIdx: number, colIdx: number) => {
+  const handleMouseDown = (e: MouseEvent<HTMLTableCellElement>, rowIdx: number, colIdx: number) => {
+    if (e.button === 2) return;
     setStartIdx(null);
     setEndIdx(null);
     setStartIdx({ startRow: rowIdx, startCol: colIdx });
   };
 
-  const handleMouseUp = (_: MouseEvent<HTMLTableCellElement>, rowIdx: number, colIdx: number) => {
+  const handleMouseUp = (e: MouseEvent<HTMLTableCellElement>, rowIdx: number, colIdx: number) => {
+    if (e.button === 2) return;
     setEndIdx({ endRow: rowIdx, endCol: colIdx });
   };
 
   if (
     startIdx !== null &&
     endIdx !== null &&
-    startIdx?.startRow + startIdx?.startCol > endIdx?.endRow + endIdx?.endCol
+    startIdx.startRow + startIdx.startCol > endIdx.endRow + endIdx.endCol
   ) {
     const temp = { startRow: endIdx.endRow, startCol: endIdx.endCol };
     setEndIdx({ endRow: startIdx.startRow, endCol: startIdx.startCol });
     setStartIdx(temp);
   }
 
-  const range = { ...startIdx, ...endIdx };
+  const range: SelectedRange = { ...startIdx, ...endIdx };
   return { range, handleMouseDown, handleMouseUp };
 };
 
