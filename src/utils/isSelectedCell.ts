90d3fdc03704395590060711cfd663d6a4372d84
lauichan <nauichan@outlook.com>
2024-08-09 16:32:16 +0900

    왼쪽에서 대각선 위로 선택할때 범위 선택이 안되던 문제 수정

--- src/utils/isSelectedCell.ts
+++ src/utils/isSelectedCell.ts
@@ -1,16 +1,23 @@
 import type { SelectedRange } from "../hooks/useSelectCells";
 
 const isSelectedCell = (range: SelectedRange, rowIdx: number, colIdx: number): boolean => {
-  return (
-    range.startRow !== undefined &&
-    range.endRow !== undefined &&
-    range.startCol !== undefined &&
-    range.endCol !== undefined &&
-    rowIdx >= range.startRow &&
-    rowIdx <= range?.endRow &&
-    colIdx >= range.startCol &&
-    colIdx <= range.endCol
-  );
+  const { startRow, endRow, startCol, endCol } = range;
+
+  if (
+    startRow === undefined ||
+    endRow === undefined ||
+    startCol === undefined ||
+    endCol === undefined
+  ) {
+    return false;
+  }
+
+  const minRow = Math.min(startRow, endRow);
+  const maxRow = Math.max(startRow, endRow);
+  const minCol = Math.min(startCol, endCol);
+  const maxCol = Math.max(startCol, endCol);
+
+  return rowIdx >= minRow && rowIdx <= maxRow && colIdx >= minCol && colIdx <= maxCol;
 };
 
 export default isSelectedCell;
