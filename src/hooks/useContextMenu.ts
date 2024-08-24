50116e5c9308da90abbedec1fecd72a1b3c1b5a3
lauichan <nauichan@outlook.com>
2024-08-24 22:45:52 +0900

    fix: useEffect 종속성 함수 리렌더링 방지

--- src/hooks/useContextMenu.ts
+++ src/hooks/useContextMenu.ts
@@ -1,14 +1,14 @@
 import type { MousePosition } from '@/components/ContextMenu/ContextMenu';
 
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const useContextMenu = () => {
   const contextMenuRef = useRef<HTMLUListElement>(null);
   const [contextMenu, setContextMenu] = useState<MousePosition>(null);
 
-  const hideContextMenu = () => {
+  const hideContextMenu = useCallback(() => {
     setContextMenu(null);
-  };
+  }, [setContextMenu]);
 
   const handleContextMenu = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>) => {
     e.preventDefault();
