801ae637f2d1eb75ac07247c24c247062f5faf76
lauichan <nauichan@outlook.com>
2024-09-06 20:19:26 +0900

    feat: 병합된 셀도 방향키로 선택가능

--- src/hooks/useArrowNavigate.ts
+++ src/hooks/useArrowNavigate.ts
@@ -3,29 +3,61 @@ import type { KeyboardEvent } from 'react';
 
 import { useRef } from 'react';
 
+type Direction = 'ArrowRight' | 'ArrowLeft' | 'ArrowDown' | 'ArrowUp';
+
 const useArrowNavigate = (table: CellInfo[][]) => {
   const cellRefs = useRef<HTMLTableCellElement[][]>([]);
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLTableCellElement>, rowIdx: number, colIdx: number) => {
-    if (e.key === 'ArrowRight') {
-      if (colIdx < table[0].length - 1) {
-        cellRefs.current[rowIdx][colIdx + 1].focus();
-      }
-    } else if (e.key === 'ArrowLeft') {
-      if (colIdx > 0) {
-        cellRefs.current[rowIdx][colIdx - 1].focus();
-      }
-    } else if (e.key === 'ArrowDown') {
-      if (rowIdx < table.length - 1) {
-        cellRefs.current[rowIdx + 1][colIdx].focus();
-      }
-    } else if (e.key === 'ArrowUp') {
-      if (rowIdx > 0) {
-        cellRefs.current[rowIdx - 1][colIdx].focus();
+  const navigateCell = (curr: [number, number], direction: Direction): [number, number] => {
+    const [rowIdx, colIdx] = curr;
+    let nextRowIdx = rowIdx;
+    let nextColIdx = colIdx;
+
+    switch (direction) {
+      case 'ArrowRight':
+        nextColIdx++;
+        break;
+      case 'ArrowLeft':
+        nextColIdx--;
+        break;
+      case 'ArrowDown':
+        nextRowIdx++;
+        break;
+      case 'ArrowUp':
+        nextRowIdx--;
+        break;
+      default:
+        return [rowIdx, colIdx];
+    }
+
+    const currentCell = table[rowIdx][colIdx];
+    if (currentCell.merged) {
+      const { rowSpan, colSpan } = currentCell.merged;
+      if (direction === 'ArrowRight') {
+        nextColIdx = colIdx + colSpan;
+      } else if (direction === 'ArrowDown') {
+        nextRowIdx = rowIdx + rowSpan;
       }
     }
+
+    const nextCell = table[nextRowIdx][nextColIdx];
+    if (nextCell && nextCell.merged) {
+      const { rowIdx, colIdx } = nextCell.merged;
+      return [rowIdx, colIdx];
+    }
+
+    nextRowIdx = Math.max(0, Math.min(nextRowIdx, table.length - 1));
+    nextColIdx = Math.max(0, Math.min(nextColIdx, table[0].length - 1));
+
+    return [nextRowIdx, nextColIdx];
   };
 
+  const handleKeyDown = (e: KeyboardEvent, rowIdx: number, colIdx: number) => {
+    if (['ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp'].indexOf(e.key) === -1) return;
+    const [nextRowIdx, nextColIdx] = navigateCell([rowIdx, colIdx], e.key as Direction);
+    cellRefs.current[nextRowIdx][nextColIdx].focus();
+    console.log(nextRowIdx, nextColIdx);
+  };
   return { cellRefs, handleKeyDown };
 };
 
